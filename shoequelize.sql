ec1d6aebc78e975563eb596265a8ab8f3278f100
Jessica <jessicamenius@gmail.com>
2020-07-11 12:52:51 -0700

    updating database

--- shoequelize.sql
+++ shoequelize.sql
@@ -3,18 +3,20 @@ CREATE database shoes_db;
 USE shoes_db;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT NOT NULL, 
+  id INT AUTO_INCREMENT, 
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  username VARCHAR (30),
+  password VARCHAR (30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE shoes (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     sku VARCHAR(6),
-    brand VARCHAR(20),
-    style VARCHAR(30),
-    gender VARCHAR(30),
+    brand VARCHAR(50),
+    style VARCHAR(50),
+    gender VARCHAR(50),
     color VARCHAR(50),
     MSRP DECIMAL(7,2),
     mktValue DECIMAL(7,2),
