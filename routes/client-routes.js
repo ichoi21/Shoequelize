5e498644d3c101670945d03d676a728102feaa03
Jessica <jessicamenius@gmail.com>
2020-07-12 18:34:18 -0700

    client routes working

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -2,16 +2,36 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-// router.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/home.html"));
-// });
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/home.html"));
+});
 
-// router.get("/edit", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/edit.html"));
-// });
+router.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/about.html"));
+});
 
-// router.get("/delete", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/delete.html"));
-// });
+router.get("/help", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/help.html"));
+});
+
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/login.html"));
+});
+
+router.get("/my_collection", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/my_collection.html"));
+});
+
+router.get("/press", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/press.html"));
+});
+
+router.get("/sign_up", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/sign_up.html"));
+});
+
+router.get("/view_all", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/view_all.html"));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./client/"));
 
-const apiRoutes = require("./lib/routes/api-routes");
+const apiRoutes = require("./routes/api-routes");
 app.use(apiRoutes);
 
-const clientRoutes = require("./lib/routes/client-routes");
+const clientRoutes = require("./routes/client-routes");
 app.use(clientRoutes);
 
 app.listen(PORT, () => console.log(`listening at: http://localhost:${PORT}`));
