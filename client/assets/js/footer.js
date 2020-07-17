e477ddd483b1ce8bce2378b339d6e3e5bf1bbc0f
Shakila Marando <marandoshakila1@gmail.com>
2020-07-17 03:10:16 -0700

    added more changes

--- client/assets/js/footer.js
+++ client/assets/js/footer.js
@@ -3,30 +3,26 @@ $(document).ready(function () {
     `
     <footer class="page-footer pink darken-4">
 
-    
-
       <div class="row container">
+
       <div class="column">
-<h6>FOLLOW US</h6>
+      <h6>FOLLOW US</h6>
     <ul id="social" style="list-style-type:none; font-size:27px; display:inline; margin-right:20px">
-    
     <li><i class="fab fa-twitter"></i></li>
     <li><i class="fab fa-facebook"></i></li>
     <li><i class="fab fa-instagram"></i></li>
     <li><i class="fa fa-pinterest-square"></i></li>
-    
     </ul>
     </div>
-
-    
 
       <div class="col s12 m3 l4 column">
         <h6 class="white-text">GET HELP</h6>
         <p class="white-text text-lighten-1">Home</p> 
-        <p class="white-text text-lighten-1">Login/p>         
+        <p class="white-text text-lighten-1">Login</p>         
         <p class="white-text text-lighten-1">Help</p>
         <p class="white-text text-lighten-1">Sell</p>
       </div>
+
       <div class="col s12 m3 l4 column">
         <h6 class="white-text">ABOUT US</h6>
         <p class="white-text text-lighten-1">About Shoequelize</p> 
@@ -34,8 +30,9 @@ $(document).ready(function () {
         <p class="white-text text-lighten-1">Our collections</p>
         <p class="white-text text-lighten-1">Contact us</p>
       </div>
-
 <div>
+<br>
+<br>
 </footer>
 
   
