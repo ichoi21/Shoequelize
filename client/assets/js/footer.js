bccd4098960c7eb347f2fcf4fbfb84f13790bfa7
Shakila Marando <marandoshakila1@gmail.com>
2020-07-17 03:00:46 -0700

    more changes to the footer

--- client/assets/js/footer.js
+++ client/assets/js/footer.js
@@ -3,32 +3,12 @@ $(document).ready(function () {
     `
     <footer class="page-footer pink darken-4">
 
-    <div class="row">
-      <div class="col s12 offset-m1 m3 offset-l1 l3">
-        <span id="followus">FOLLOW US</span><span></span>
-        <span id="followus"><a class="blue-grey-text text-lighten-1" href="#!">
-        <span style="font-size: 36px; color: White;">
-        <i class="fab fa-twitter"></i></span></a></span>
+    
 
-        <span id="followus"><a class="blue-grey-text text-lighten-1" href="#!">
-        <span span style="font-size: 36px; color: White;">
-        <i class="fab fa-facebook"></i></span></a></span>
-
-        <span id="followus"><a class="blue-grey-text text-lighten-1" href="#!">
-        <span style="font-size: 36px; color: White;">
-        <i class="fab fa-instagram"></i></span></a></span>
-      </div>
-      <div class="col s12 m3 l4">
-        <h6 class="white-text">ABOUT US</h6>
-        <p class="white-text text-lighten-1">About Shoequelize</p> 
-        <p class="white-text text-lighten-1">Press</p>         
-        <p class="white-text text-lighten-1">Our collections</p>
-        <p class="white-text text-lighten-1">Contact us</p>
-      </div>
-
-<div class="container">
+      <div class="row container">
+      <div class="column">
 <h6>FOLLOW US</h6>
-    <ul id="social" style="list-style-type:none; font-size:36px; display:inline; margin-right:20px">
+    <ul id="social" style="list-style-type:none; font-size:27px; display:inline; margin-right:20px">
     
     <li><i class="fab fa-twitter"></i></li>
     <li><i class="fab fa-facebook"></i></li>
@@ -37,7 +17,27 @@ $(document).ready(function () {
     
     </ul>
     </div>
-    </footer>
+
+    
+
+      <div class="col s12 m3 l4 column">
+        <h6 class="white-text">GET HELP</h6>
+        <p class="white-text text-lighten-1">Home</p> 
+        <p class="white-text text-lighten-1">Login/p>         
+        <p class="white-text text-lighten-1">Help</p>
+        <p class="white-text text-lighten-1">Sell</p>
+      </div>
+      <div class="col s12 m3 l4 column">
+        <h6 class="white-text">ABOUT US</h6>
+        <p class="white-text text-lighten-1">About Shoequelize</p> 
+        <p class="white-text text-lighten-1">Press</p>         
+        <p class="white-text text-lighten-1">Our collections</p>
+        <p class="white-text text-lighten-1">Contact us</p>
+      </div>
+
+<div>
+</footer>
+
   
     <div class="footer-copyright pink lighten-5">
       <div class="row">
