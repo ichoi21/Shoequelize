4dd0dcaf3289f853c60fe7ee89bf829a13c1efdd
myhkas <mmyhkas@gmail.com>
2020-07-16 21:25:12 -0700

    added search shoes func for none-user

--- client/assets/js/landing.js
+++ client/assets/js/landing.js
@@ -43,6 +43,57 @@ $(document).ready(function () {
   $("#registerCancel").on("click", function () {
     registerModalInstance.close();
   });
+
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  stockx();
+
+  function stockx() {
+    $("#btnSearch").on("click", function (e) {
+      e.preventDefault();
+      query = $("#query").val();
+      shoeBrand = $("#shoeBrand").val();
+      shoeYr = $("#shoeYr").val();
+      shoeGdr = $("#shoeGdr").val();
+      console.log(shoeBrand, shoeYr, query, shoeGdr);
+      stockXSearch();
+    });
+  }
+  function stockXSearch() {
+    $.ajax({
+      type: "GET",
+      url: `https://stockx.com/api/browse?&_search=${query}&year=${shoeYr}&brand=${shoeBrand}&gender=${shoeGdr}`,
+      dataType: "json",
+    }).then(function (shoe) {
+      for (let i = 0; i < 5; i++) {
+        let year = shoe.Products[i].year;
+        let brand = shoe.Products[i].brand;
+        let PID = shoe.Products[i].styleId;
+        let name = shoe.Products[i].shoe;
+        let gender = shoe.Products[i].gender;
+        let colorway = shoe.Products[i].colorway;
+        let msrp = shoe.Products[i].retailPrice;
+        let mV = shoe.Products[i].market.lastSale;
+        let img = shoe.Products[i].media.smallImageUrl;
+        $("#result").append(`
+              <div>
+                      <h2>
+                      ${year} ${gender} ${brand}: ${name}
+                      </h2>
+                      <h4>
+                        ${PID} - ${formatter.format(msrp)}
+                      </h4>
+                      <h3> ${colorway}</h3>
+                      <h3> Current Value: ${formatter.format(mV)}</h3>
+                      <img src="${img}" alt="" />
+                    </div>
+              `);
+      }
+    });
+  }
 });
 
 const loginUser = (userObj) => {
